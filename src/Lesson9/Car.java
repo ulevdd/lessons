d9109e65f1fab9e7d374306d8e0abc521fd22ef2
Dul001 <ulevdd@gmail.com>
2025-12-15 17:38:02 +0300

    15.12

--- src/Lesson9/Car.java
+++ src/Lesson9/Car.java
@@ -5,7 +5,7 @@ public class Car {
     String color;
     String engine;
     static int count;
-
+    static int pricce;
 
     Car (String color, String engine){
 
@@ -17,11 +17,29 @@ public class Car {
     public void showColor (){
 
         System.out.println("Цвет машины: " + color);
+        System.out.println("Машина №: " + count);
     }
     public  void  changeColor (String color1){
-        System.out.println("Цвет измениелся: ");
-        int price = 5000;
+
+        pricce = 5000;
+        System.out.println("Цена до: "+ pricce);
         color=color1;
-        price+=1000;
+        System.out.println("Цвет измениелся: "+ color);
+        pricce+=10000;
+        System.out.println("Цена после: "+ pricce);
+    }
+
+    static void main() {
+        Car c1 = new Car("Yellow", "v6");
+        System.out.println(c1.color);
+        System.out.println("");
+        c1.changeColor("Red");
+        c1.changeColor("blue");
+        c1.showColor();
+        Car c2 = new Car("asd", "dasd");
+        c2.showColor();
+        c2.changeColor("Red");
+        c2.showColor();
+
     }
 }
